e6aba013a39f7d861354978ac1881ec3c141f141
katros <kasia.roszak@gmail.com>
2018-09-06 17:20:40 +0200

    fixed markers again

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,35 +1,34 @@
 $(document).ready(function() {
     start();
     let myIcon = L.icon({
-        iconUrl: "../stylesheets/marker-yellow.svg",
+        iconUrl: '../stylesheets/marker-yellow.svg',
         iconSize: [38, 95],
         iconAnchor: [22, 94],
         popupAnchor: [-3, -76]
     });
 
     let othersIcon = L.icon({
-        iconUrl: "../stylesheets/marker-black.svg",
+        iconUrl: '../stylesheets/marker-black.svg',
         iconSize: [38, 95],
         iconAnchor: [22, 94],
         popupAnchor: [-3, -76]
     });
 
     let followingIcon = L.icon({
-        iconUrl: "../stylesheets/marker-blue.svg",
+        iconUrl: '../stylesheets/marker-blue.svg',
         iconSize: [38, 95],
         iconAnchor: [22, 94],
         popupAnchor: [-3, -76]
     });
 
     let poly;
-    let map = L.map("map").locate({ setView: true, maxZoom: 17 }),
+    let map = L.map('map').locate({ setView: true, maxZoom: 17 }),
         geocoder = L.Control.Geocoder.nominatim(),
         control = L.Control.geocoder({
             geocoder: geocoder,
-            // showResultIcons: true,
             defaultMarkGeocode: false
         })
-            .on("markgeocode", function(e) {
+            .on('markgeocode', function(e) {
                 if (poly) {
                     poly.remove();
                 }
@@ -46,13 +45,13 @@ $(document).ready(function() {
         marker;
 
     L.tileLayer(
-        "https://maps.tilehosting.com/styles/positron/{z}/{x}/{y}.png?key=9rAT960ktqr7deCTc1f0",
+        'https://maps.tilehosting.com/styles/positron/{z}/{x}/{y}.png?key=9rAT960ktqr7deCTc1f0',
         {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }
     ).addTo(map);
 
-    map.on("click", function(e) {
+    map.on('click', function(e) {
         let lat = e.latlng.lat;
         let lng = e.latlng.lng;
 
@@ -67,7 +66,7 @@ $(document).ready(function() {
             let city_district = results[0].properties.address.city_district;
 
             let r = results[0];
-            console.log(results[0]);
+
             if (r) {
                 marker = L.marker(r.center, { icon: myIcon })
                     .bindPopup(r.name)
@@ -95,48 +94,45 @@ $(document).ready(function() {
         let radius = e.accuracy / 2;
         L.circle(e.latlng, radius).addTo(map);
     }
-    map.on("locationfound", onLocationFound);
+    map.on('locationfound', onLocationFound);
 
     function start() {
-        axios.get("/protected/stories").then(result => {
-            console.log(result);
+        axios.get('/protected/stories').then(result => {
             let user = result.data.user;
             let following = result.data.followingList;
             result.data.stories.forEach(story => {
-                let date = moment(story.created_at).format("lll");
-                let markerHtml = `<a href="/protected/user/${story.username}"><p>${
-                    story.username
-                }</p></a>
+                let date = moment(story.created_at).format('lll');
+                let markerHtml = `<a href="/protected/user/${story.username}">
+                <p>${story.username}</p></a>
                 <div class="display-story-container"><p>${story.story}</p>
-                <p>${story.address.street ? story.address.street : ""}</p>
-                <p>${story.address.city_district ? story.address.city_district : ""}</p>
-                <p>${story.address.town ? story.address.town : ""}</p>
-                <p>${story.address.city ? story.address.city : ""}</p>
-                <p>${story.address.county ? story.address.county : ""}</p>
-                <p>${story.address.country ? story.address.country : ""}</p
+                <p>${story.address.street ? story.address.street : ''}</p>
+                <p>${story.address.city_district ? story.address.city_district : ''}</p>
+                <p>${story.address.town ? story.address.town : ''}</p>
+                <p>${story.address.city ? story.address.city : ''}</p>
+                <p>${story.address.county ? story.address.county : ''}</p>
+                <p>${story.address.country ? story.address.country : ''}</p
                 <p>${date}</p>
                 </div>`;
                 if (user === story.username) {
-                    console.log("YIS");
                     new L.marker([story.location.lat, story.location.lng], { icon: myIcon })
                         .bindPopup(markerHtml)
                         .addTo(map);
                 } else {
-                    console.log(following);
+                    new L.marker([story.location.lat, story.location.lng], { icon: othersIcon })
+                        .bindPopup(markerHtml)
+                        .addTo(map);
+                }
 
-                    const followingUser = following.find(e => story.username);
-
-                    if (followingUser) {
-                        new L.marker([story.location.lat, story.location.lng], {
-                            icon: followingIcon
-                        })
-                            .bindPopup(markerHtml)
-                            .addTo(map);
-                    } else {
-                        new L.marker([story.location.lat, story.location.lng], { icon: othersIcon })
-                            .bindPopup(markerHtml)
-                            .addTo(map);
-                    }
+                if (following) {
+                    following.forEach(e => {
+                        if (e === story.username) {
+                            new L.marker([story.location.lat, story.location.lng], {
+                                icon: followingIcon
+                            })
+                                .bindPopup(markerHtml)
+                                .addTo(map);
+                        }
+                    });
                 }
             });
         });
