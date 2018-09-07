cca7b28d5ef91456dc80c497c95dc40b328ae658
katros <kasia.roszak@gmail.com>
2018-09-07 09:23:19 +0200

    added css to user pages

--- public/javascripts/follow.js
+++ public/javascripts/follow.js
@@ -1,25 +1,25 @@
 // follow button
 $(document).ready(function() {
-    let username = $("#username-title").html();
-    $(".follow")
-        .unbind("click")
-        .bind("click", () => {
+    let username = $('#username-title').html();
+    $('.follow')
+        .unbind('click')
+        .bind('click', () => {
             $.ajax({
                 // AJAX
                 url: `/protected/follow/${username}`,
-                type: "GET",
-                dataType: "json",
+                type: 'GET',
+                dataType: 'json',
                 success: function(data, status) {
-                    console.log("at ajax", data);
+                    console.log('at ajax', data);
 
                     //if it is NOT following = -1
 
                     if (data.following.indexOf(username) === -1) {
-                        $(".follow").addClass("active");
-                        $(".follow").text("Follow");
+                        $('.follow').addClass('active');
+                        $('.follow').text('FOLLOW');
                     } else {
-                        $(".follow").removeClass("active");
-                        $(".follow").text("Following");
+                        $('.follow').removeClass('active');
+                        $('.follow').text('FOLLOWING');
                     }
                 }
             });
